e1013eb048a6f3effb4da87854c34429ec4fc7e6
Kirk <kirk@ColPossoms.(none)>
2013-08-25 13:15:02 -0400

    Well it's kinda a game

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
 var gs = require('GameServer');
 var MatchState = "Joining";
+var MATCH_TIME = 10000;
+var Timer = MATCH_TIME, EndTimer = 3000;
+var Parts = [];
 
 gs.Server(
 	onJoin,
 	onQuit,
 	onUpdate,
 	onMove,
-	onCheckIn
+	onCheckIn,
+	onServerEmpty
 );
 
 function rand(min, max){
   return (Math.random() * max - min) + min;
 }
 
-function onJoin(name){
+function createParts(){
+	Parts = [];
+	var types = ['engine','armor','laser'];
+	for(var i=40; i--;){
+		Parts.push({
+			ID: i,
+			dx: rand(-1, 1),
+			dy: rand(-1, 1),
+			x: rand(-999, 999),
+			y: rand(-999, 999),
+			type: types[i%types.length]
+		});
+	}
+}
+
+function positionPlayers(players){
+	for(var i = players.length; i--;){
+		var r = (i / players.length) * Math.PI * 2;
+		var p = players[i];
+		if(!p) continue;
+		players[i].x = Math.cos(r) * 100; players[i].y = Math.sin(r) * 100;
+		players[i].dx = players[i].dy = 0;
+		players[i].parts = [];
+		players[i].Ready = false;
+	}
+
+	return players;
+}
+
+function onJoin(socket, name){
+	if(MatchState != "Joining"){
+		socket.emit('error', "Can't join a match that is in progress. Try again.");
+		socket.disconnect('unauthorized');
+		return null;
+	}
+
 	console.log(name + ' has joined the game!');
 	var player = {
 		ID: -1,
@@ -24,16 +63,23 @@ function onJoin(name){
 	return player;
 }
 
-function onQuit(player){
-
+function onQuit(player, playerCount){
+	if(playerCount == 0){
+		MatchState = "Joining";
+		Timer = MATCH_TIME;
+		EndTimer = 3000;
+	}
 }
 
 function onMove(player, moveData){
-	console.log("ONMOVE");
 	switch(MatchState){
 		case "Joining":
-			console.log(moveData.Ready);
 			player.Ready = moveData.Ready;
+			break;
+		case "Collecting":
+			console.log("dx: " + moveData.dx + " dy: " + moveData.dy);
+			player.dx = moveData.dx;
+			player.dy = moveData.dy;
 			break;
 	}
 }
@@ -42,8 +88,156 @@ function onCheckIn(player){
 
 }
 
+function onServerEmpty(){
+	MatchState = "Joining";
+	Timer = MATCH_TIME;
+	EndTimer = 3000;
+	console.log('Switching to Joining');
+}
+
+function getPlayerPartCount(p){
+	var playerParts = {Engines: 0, Armor: 0, Lasers: 0};
+	for(var j = p.parts.length; j--;){
+		switch(p.parts[j].type){
+			case "armor":
+				++playerParts.Armor;
+				break;
+			case "engine":
+				++playerParts.Engines;
+				break;
+			case "laser":
+				++playerParts.Lasers;
+				break;
+		}
+	}
+	return playerParts;
+}
+
+function BuildingStage(Players,data){
+	for(var i = Players.length; i--;){
+		var p = Players[i];
+		if(!p){
+			continue;
+		}
+
+		data.Players.push({
+			ID: p.ID,
+			Name: p.Name,
+			Parts: getPlayerPartCount(p)
+		});
+	}
+	data.ServerMessage = "Build a ship! " + Math.floor((Timer -= dt) / 1000) + "...";
+
+	return allReady && !noPlayers;
+}
+
+function CollectingStage(Players, data, dt) {
+	var allOut = true;
+	data.GrabbedParts = []; data.Parts = [];
+
+	if(Timer - dt < 0){
+		// DONE
+		data.ServerMessage = 'Collecting phase finished!!!';
+		data.ServerMessage += '<br/><br/>';
+
+		for(var i = Players.length; i--;){
+			var p = Players[i];
+			if(!p){
+				continue;
+			}
+
+			var pp = getPlayerPartCount(p);
+			data.ServerMessage += p.Name + " - " + pp.Engines + "xEngines, " + pp.Armor + "xArmor, " + pp.Lasers + "xLasers";
+			data.ServerMessage += "<br/><br/>";
+		}
+
+		EndTimer -= dt;
+
+		if(EndTimer < 0){
+			return true;
+		}
+
+		return false;
+	}
+
+
+	var rad = 32;
+	var partRadius = rad * rad;
+
+	// update players
+	for(var i = Players.length; i--;){
+		var p = Players[i];
+		if(!p){
+			continue;
+		}
+		var dx = p.dx * dt;
+		var dy = p.dy * dt;
+
+		if(p.x + dx < 1000 && p.x + dx > -1000)
+			p.x += dx;
+
+		if(p.y + dy < 1000 && p.y + dy > -1000)
+			p.y += dy;
+
+		p.dx *= 0.5; p.dy *= 0.5;
+
+		for(var j = Parts.length; j--;){
+			var part = Parts[j];
+			if(!part) continue;
+			dx = p.x - part.x; dy = p.y - part.y;
+			var r = dx * dx + dy * dy;
+			if(r <= partRadius){
+				p.parts.push(part);
+				//data.GrabbedParts.push(part);
+				part.x = -10000;
+				part.y = -10000;
+				part.dx = part.dy = 0;
+			}
+		}
+
+		var playerParts = getPlayerPartCount(p);
+
+		data.Players.push({
+			ID: p.ID,
+			Name: p.Name,
+			x: Math.floor(p.x),
+			y: Math.floor(p.y),
+			parts: playerParts
+		});
+		allOut = false;
+	}
+
+	var partData = [];
+	for(var i = Parts.length; i--;){
+		var p = Parts[i];
+
+		if(!p) continue;
+
+		var dx = p.dx * dt, dy = p.dy * dt;
+
+		if(p.x + dx > 1000 || p.x + dx < -1000)
+			p.dx = -p.dx;
+
+		if(p.y + dy > 1000 || p.y + dy < -1000)
+			p.dy = -p.dy;
+
+		p.x += p.dx * dt;
+		p.y += p.dy * dt;
+
+		partData.push({
+			ID: p.ID,
+			x: Math.floor(p.x),
+			y: Math.floor(p.y),
+			type: p.type
+		})
+	}
+
+	data.Parts = partData;
+	data.ServerMessage = "Collect parts! " + Math.floor((Timer -= dt) / 1000) + "...";
+}
+
 function JoiningStage(Players, data) {
-	var allReady = true;
+	var allReady = true, noPlayers = true;
 	for(var i = Players.length; i--;){
 		var p = Players[i];
 		if(!p){
@@ -53,6 +247,7 @@ function JoiningStage(Players, data) {
 			allReady = false;
 		}
 
+		noPlayers = false;
 		data.Players.push({
 			ID: p.ID,
 			Name: p.Name,
@@ -60,8 +255,9 @@ function JoiningStage(Players, data) {
 		});
 	}
 
-	if(Players.length < 2){
+	if(Players.length < 1){
 		data.ServerMessage = "Waiting for more players...";
+		return false;
 	}
 	else if(!allReady){
 		data.ServerMessage = "Waiting for players...";
@@ -70,7 +266,7 @@ function JoiningStage(Players, data) {
 		data.ServerMessage = "Starting game!";
 	}
 
-	return allReady;
+	return allReady && !noPlayers;
 }
 
 function onUpdate(Players, dt){
@@ -80,11 +276,24 @@ function onUpdate(Players, dt){
     	case "Joining":
     		
     		if(JoiningStage(Players, data)){
+    			MatchState = "Collecting";
+    			console.log('Switching to collection');
+    			Players = positionPlayers(Players);
+    			createParts();
 
     		}
     		break;
-    	case "StartingCollect":
+    	case "Collecting":
+    		if(CollectingStage(Players, data, dt)){
+    			Timer = MATCH_TIME;
+    			EndTimer = 3000;
+    			MatchState = "Joining";
+    		}
+    		break;
+    	case "Building":
+    		if(BuildingStage(Players, data)){
 
+    		}
     		break;
     }
 
--- scripts/client.js
+++ scripts/client.js
@@ -1,4 +1,4 @@
-  var server = 'http://localhost';
+  var server = 'http://192.168.1.4';
   var socket = null;
   var loggedIn = false;
   var player   = null;
@@ -6,6 +6,8 @@
   var piover2 = Math.PI / 2;
 
   var MatchState = "none";
+  var CollectionPlayers = [], CollectionParts = [];
+  var CamX = 0, CamY = 0;
 
 function UpdateBackground(x, y){
 	divs = ['#StarsFar','#StarsMed','#StarsNear'];
@@ -39,12 +41,18 @@ for(var i = 1; i <= mat.cols(); i++){
 function cleanUpMatchState(){
 	switch(MatchState){
 		case "Joining":
-			//TODO
+			var lobby = $('#Box');
+			lobby.fadeOut(500, function(){
+				lobby.remove();
+			});
+			break;
+		case "Collecting":
+			$('#body').remove();
 			break;
 	}
 }
 
-function InitMatchState() {
+function InitMatchState(data) {
 	switch(MatchState){
 		case "Joining":
 			var box = $('<div id="Box" style="display:none;"/>')
@@ -53,7 +61,93 @@ function InitMatchState() {
 				.appendTo(box);
 			var con = $('<div id="LobbyBox" class="Container">Lobby</div>').appendTo(box);
 			break;
+		case "Collecting":
+			$('<div id="body" />').appendTo($('body'));
+			break;
+		case "Building":
+			var div = $('<div id="body" />').appendTo($('body'));
+
+			for (var i = data.Players.length - 1; i--;) {
+				Things[i]
+			};
+
+			var eng = $('<div class="partButton">' + '</div>');
+			eng.css('background', 'url("images/engine.png")');
+			var arm = $('<div class="partButton">'+'</div>');
+			arm.css('background', 'url("images/armor.png")');
+			var laz = $('<div class="partButton">'+'</div>');
+			laz.css('background', 'url("images/laser.png")');
+
+
+
+			div.append(eng).append(arm).append(laz);
+			div.appendTo($('body'));
+			break;
 	}
+}
+
+function UpdateCollectionRound(data){
+	var players = data.GameData.Players;
+	var parts   = data.GameData.Parts;
+	var quitters = data.PlayersQuit;
+	var taken    = data.GameData.GrabbedParts;
+	var classes = ['phatty phattyEast','phatty phattyWest','phatty phattyNorth','phatty phattySouth'];
+
+	for(var i=players.length; i--;){
+		var p = players[i];
+		var div = $('#' + p.ID);
+
+		if(!div.length){
+			var c = classes[Math.floor(Math.random() * classes.length)];
+			div = $('<div id="' + p.ID + '" class="' + c + '">' + p.Name + '</div>')
+				.appendTo($('#body'));
+
+			div[0].x = p.x;
+			div[0].y = p.y;
+
+			if(p.ID == player.ID){
+				 div.click(function(){
+					socket.emit("RequestMove", {Ready: !player.Ready});
+				});
+			}
+		}
+
+		if(p.ID == player.ID){
+			player = p;
+		}
+
+	}
+
+	for(var i=quitters.length; i--;){
+		var p = quitters[i];
+		$('#' + p.ID).remove();
+	}
+
+	for(var i=parts.length; i--;){
+		var p = parts[i];
+		var div = $('#part' + p.ID);
+
+		if(!div.length){
+			div = $('<div id="part' + p.ID + '" class="part"></div>')
+				.appendTo($('#body'));
+			div.css('background', 'url("images/' + p.type + '.png")');
+			div[0].x = p.x;
+			div[0].y = p.y;
+		}
+	}
+
+
+	for(var i=taken.length; i--;){
+		var p = taken[i];
+		var div = $('#part' + p.ID);
+		div.remove();
+	}
+
+
+	$('#Info').html(data.GameData.ServerMessage);
+
+	CollectionPlayers = players;
+	CollectionParts = parts;
 }
 
 function UpdateLobby(data){
@@ -109,7 +203,7 @@ function start(name, info){
 	});
 
 	socket.on('error', function(msg){
-		//info.text('Error! ' + msg);
+		info.text(msg);
 	});
 
 	socket.on('PushUpdate', function(data){
@@ -117,12 +211,15 @@ function start(name, info){
 		if(gd.MatchState != MatchState){
 			cleanUpMatchState();
 			MatchState = gd.MatchState;
-			InitMatchState();
+			InitMatchState(data);
 		}
 
 		switch(MatchState){
 			case "Joining":
 				UpdateLobby(data);
+				break;
+			case "Collecting":
+				UpdateCollectionRound(data);
 				break;
 		}		
 	});
@@ -176,14 +273,126 @@ OPfoundation.Init(null, function(dt){
 	// update
 	if(loggedIn){
 		socket.emit('RequestCheckIn', player.Name);
-
-		var div = $('#' + player.ID);
-
 	}
 	
-	if(MatchState=="Joining" || !loggedIn){
-		bgSpinny += 0.00001 * dt;
-		UpdateBackground(1000 * Math.cos(bgSpinny), 1000 * Math.sin(bgSpinny));
+	switch(MatchState){
+		case "none":
+		case "Joining":
+			if(!loggedIn){
+				bgSpinny += 0.00001 * dt;
+				UpdateBackground(1000 * Math.cos(bgSpinny), 1000 * Math.sin(bgSpinny));
+			}
+			break;
+		case "Collecting":
+			if(loggedIn){
+				var dx = 0, dy = 0;
+				if($KB.IsKeyDown(87)){
+					dy = -0.25;
+				}
+
+				if($KB.IsKeyDown(83)){
+					dy = 0.25;
+				}
+
+				if($KB.IsKeyDown(65)){
+					dx = -0.25;
+				}
+
+				if($KB.IsKeyDown(68)){
+					dx = 0.25;
+				}
+
+				socket.emit('RequestCheckIn', player.Name);
+
+				if(dx != 0 || dy != 0){
+					var len = Math.sqrt(dx * dx + dy * dy);
+					dx /= len; dy /= len;
+
+					socket.emit('RequestMove', {
+						dx: dx,
+						dy: dy,
+					});
+				}
+			}
+
+			CamX += Math.floor((player.x - CamX) / 10);
+			CamY += Math.floor((player.y - CamY) / 10);
+
+			for(var i = CollectionPlayers.length; i--;){
+				var p = CollectionPlayers[i];
+				if(!p) continue;
+				var div = $('#' + p.ID);
+				if(div.length){
+
+					var dx = p.x - div[0].x, dy = p.y - div[0].y;
+
+					div[0].x += Math.floor((p.x - div[0].x) / 10);
+					div[0].y += Math.floor((p.y - div[0].y) / 10);			
+
+
+					var mat = Matrix.I(2);
+
+					mat = mat.augment([
+						[Math.floor(div[0].x - 64 - CamX + (window.innerWidth  >> 1))],
+						[Math.floor(div[0].y - 64 - CamY + (window.innerHeight >> 1))]
+					]);
+
+					var mstr = CSSMatrix(mat);		
+
+					div.removeClass();
+					if(Math.abs(dx) > Math.abs(dy)){
+						if(dx > 0){ // east
+							div.addClass('phatty phattyEast');
+						}
+						else{ // west
+							div.addClass('phatty phattyWest');
+						}
+					}
+					else{
+						if(dy > 0){ // south
+							div.addClass('phatty phattySouth');
+						}
+						else{ // north
+							div.addClass('phatty phattyNorth');
+						}
+					}
+
+					div.css('transform', mstr)
+					   .css('-ms-transform', mstr)
+					   .css('-webkit-transform', mstr);
+				}
+			}
+
+			bgSpinny += 0.01 * dt;
+			for(var i = CollectionParts.length; i--;){
+				var p = CollectionParts[i];
+				if(!p) continue;
+				var div = $('#part' + p.ID);
+				if(div.length){
+
+					var dx = p.x - div[0].x, dy = p.y - div[0].y;
+
+					div[0].x += Math.floor((p.x - div[0].x) / 10);
+					div[0].y += Math.floor((p.y - div[0].y) / 10);			
+
+
+					var mat = Matrix.Rotation(bgSpinny);
+
+					mat = mat.augment([
+						[Math.floor(div[0].x - 64 - CamX + (window.innerWidth  >> 1))],
+						[Math.floor(div[0].y - 64 - CamY + (window.innerHeight >> 1))]
+					]);
+
+					var mstr = CSSMatrix(mat);		
+
+					div.css('transform', mstr)
+					   .css('-ms-transform', mstr)
+					   .css('-webkit-transform', mstr);
+				}
+			}
+			UpdateBackground(-CamX, -CamY);
+
+			break;
 	}
 
 });
